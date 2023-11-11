0112ad7dc70bf7bae4fa5312eeb41c4230d6e86a
jesus <jcd010795@gmail.com>
2023-11-11 15:49:34 -0600

    Creating the application 'core'

--- core/views.py
+++ core/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('hello world')
+
+
+def about(request):
+    return HttpResponse('about page')
+
+
+def services(request):
+    return HttpResponse('services page')
+
+
+def store(request):
+    return HttpResponse('store pages')
+
+
+def contact(request):
+    return HttpResponse('contact page')
+
+
+def blog(request):
+    return HttpResponse('blog page')
+
+
+def sample(request):
+    return HttpResponse('sample page')
