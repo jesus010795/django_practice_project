67f0aee254ae2e6d33d4f0343a8a8f51debbb822
jesus <jcd010795@gmail.com>
2023-12-30 15:53:44 -0600

    Pages app: urls and templates

--- pages/urls.py
+++ pages/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.sample, name='sample'),
-    path('pages/<int:page_id>/', views.category, name='page'),
+    path('<int:page_id>/', views.page, name='page'),
     
     # path('',)
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Page
 # Create your views here.
 
-def sample(request, page_id):
+def page(request, page_id):
     page = get_object_or_404(Page, id=page_id)
-    return render(request,'pages/sample.html', {'page': page})
+    return render(request,'pages/page.html', {'page': page})
+
+def sample(request):
+    return render(request,'pages/sample.html')
