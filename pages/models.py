b110ce8d00db7ffb24124680bd52e39491c016fc
jesus <jcd010795@gmail.com>
2023-12-31 14:26:48 -0600

    Admin: Reversing admin urls

--- pages/models.py
+++ pages/models.py
@@ -5,13 +5,14 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(verbose_name="Titulo", max_length=100)
     content = models.TextField(verbose_name="Contenido")
+    order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualizacion")
 
     class Meta:
         verbose_name = 'Pagina'
         verbose_name_plural = 'Paginas'
-        ordering = ['title']
+        ordering = ['order', 'title']
 
     def __str__(self):
         return self.title
